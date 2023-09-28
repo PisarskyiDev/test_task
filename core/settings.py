0508896105fff490825d8ede0110a028370d4f9e
soobig666 <darkvader.ukr.net@gmail.com>
2023-09-28 10:23:05 +0300

    Install python-dotenv && Freeze requirements.txt && Update settings.py (set SECRET_KEY fgrom .env & Add "rest_framework" to INSTALLED_APPS & add "localhost" to ALLOWED_HOSTS & INTERNAL_IPS)

--- core/settings.py
+++ core/settings.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +23,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-vqcjy!49bvgmfc@=@k!w_+79d_*oelj^fytt22tw5f8mfi=6a!"
+SECRET_KEY = os.getenv("SECRET_KEY", "!!Default_Secret_Key_what_should_be_changed!!")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "localhost",
+]
+
+INTERNAL_IPS = [
+    "localhost",
+]
 
 
 # Application definition
@@ -37,6 +46,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
